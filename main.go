d7272f9bed2b581fee1c0e3a35d62d2573467b77
Will Liu <William.Q.Liu@gmail.com>
2017-05-22 16:28:31 -0600

    Basic go web server runs on port :8080 of localhost

---
+++ main.go
@@ -0,0 +1,22 @@
+// Main app
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there")
+}
+
+func main() {
+	log.Println("Log: main app is running")
+	http.HandleFunc("/", indexHandler)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
