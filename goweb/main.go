b7de9665a4c022fb5a162bbad150fbdffa23efba
Will Liu <William.Q.Liu@gmail.com>
2017-05-27 08:04:09 -0600

    Make multiple handlers

--- goweb/main.go
+++ goweb/main.go
@@ -9,13 +9,19 @@ import (
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there")
+	fmt.Fprintf(w, "Hi there, I'm Will.")
+	log.Println("Log: handler request")
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello again")
 	log.Println("Log: handler request")
 }
 
 func main() {
 	log.Println("Log: main app is running")
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/hello/", helloHandler)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal(err)
