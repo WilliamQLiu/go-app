9330812d8babaa6baab4a4e4436c5dc6885cb1ad
Will Liu <William.Q.Liu@gmail.com>
2017-06-06 09:10:32 -0600

    hello handler parses request form

--- goweb/main.go
+++ goweb/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -14,7 +15,17 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello again")
+	fmt.Fprintf(w, "Hello again") // send data to client side
+
+	// print the parsed form on server side
+	r.ParseForm()                       // parse arguments, e.g. /hello/?url_long=111&url_long=222
+	fmt.Println("path", r.URL.Path)     // `/hello/`
+	fmt.Println("scheme", r.URL.Scheme) // scheme
+	fmt.Println(r.Form["url_long"])     // [111 222]
+	for k, v := range r.Form {
+		fmt.Println("key:", k)                    // key: url_long
+		fmt.Println("val:", strings.Join(v, " ")) // val: 111 222
+	}
 	log.Println("Log: handler request")
 }
 
