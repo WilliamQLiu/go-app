f1743e56bc3b00a9608ffdb43d81e9e50b06ccdb
Will Liu <William.Q.Liu@gmail.com>
2017-06-06 13:49:25 -0600

    Add token to login form

--- goweb/login.go
+++ goweb/login.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
 	"html/template"
+	"io"
 	"log"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/pressly/chi"
 )
@@ -22,14 +26,37 @@ func (rs loginResource) Routes() chi.Router {
 }
 
 func (rs loginResource) New(w http.ResponseWriter, r *http.Request) {
+	crutime := time.Now().Unix()
+	hash := md5.New()
+	io.WriteString(hash, strconv.FormatInt(crutime, 10))
+	token := fmt.Sprintf("%x", hash.Sum(nil))
+
 	t, _ := template.ParseFiles("templates/login.gtpl")
-	t.Execute(w, nil)
+	t.Execute(w, token) // pass token object to template
 	log.Println("Log: loginResource New route")
 }
 
 func (rs loginResource) Create(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
-	fmt.Println("username:", r.Form["username"])
-	fmt.Println("password:", r.Form["password"])
+
+	token := r.Form.Get("token")
+	var username string = template.HTMLEscapeString(r.Form.Get("username"))
+	var password string = template.HTMLEscapeString(r.Form.Get("password"))
+
+	if token != "" {
+		// Check token validity
+		fmt.Println("Token is" + token)
+	} else {
+		// Error if no token
+		fmt.Println("No Token")
+	}
+
+	if len(username) == 0 || len(password) == 0 {
+		fmt.Println("No username or password given")
+	}
+
+	fmt.Println("username:", username)
+	fmt.Println("password:", password)
 	log.Println("Log: loginResource Create route")
+
 }
