06d01d33d277ec62cb7033bc9a59859d36eb7a6a
Will Liu <William.Q.Liu@gmail.com>
2017-06-12 11:02:27 -0600

    Refactor code structure to MVC dirs

---
+++ goweb/vendor/app/util/helper.go
@@ -0,0 +1,14 @@
+package utils
+
+import (
+	"os"
+)
+
+// GetKey: helper func to get a 'key', if none then returns 'fallback' string
+func GetKey(key, fallback string) string {
+	value, present := os.LookupEnv(key)
+	if !present {
+		return fallback
+	}
+	return value
+}
