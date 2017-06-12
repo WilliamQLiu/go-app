e39546966b70c81b5232bbdfa7a95830eb6926e4
Will Liu <William.Q.Liu@gmail.com>
2017-06-12 11:15:09 -0600

    Rename packages from plural to singular

--- goweb/vendor/app/util/helper.go
+++ goweb/vendor/app/util/helper.go
@@ -1,10 +1,10 @@
-package utils
+package util
 
 import (
 	"os"
 )
 
-// GetKey: helper func to get a 'key', if none then returns 'fallback' string
+// GetKey : helper func to get a 'key', if none then returns 'fallback' string
 func GetKey(key, fallback string) string {
 	value, present := os.LookupEnv(key)
 	if !present {
